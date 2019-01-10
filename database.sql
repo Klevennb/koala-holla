f372e37f36e64c066755d3594ef2ab328908f0a8
Matt Kleven <klevennb115@gmail.com>
2019-01-10 12:57:08 -0600

    added koalas

--- database.sql
+++ database.sql
@@ -6,3 +6,9 @@ CREATE TABLE "koalas" (
 	"ready_to_transfer" BOOLEAN DEFAULT FALSE,
 	"notes" VARCHAR(60) DEFAULT ''
 );
+
+INSERT INTO "koalas" ("id", "name", "gender", "age", "ready_to_transfer", "notes")
+VALUES ('Doug', 'M', 3, false, 'Stoic'),
+('Chad', 'F', 19, true, "Conspiracy theorist"),
+('Patty Wagstaff', 'M', 1, false, 'Eucalyptus addiction'),
+('Nemoy', 'F', 9, false, 'Always happy');
