283eb60f4d08719d4459ec4e6ee9a25de62d65db
Matt Kleven <klevennb115@gmail.com>
2019-01-10 13:28:10 -0600

    added GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -16,8 +16,35 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
-} // end getKoalas
+  $.ajax({
+    method: 'GET',
+    url: '/koalas',
+  }).then(function (response) {
+    const listOfKoalas = response;
+    $('#viewKoalas').empty();
+    for (koala of listOfKoalas) {
+      
+      $('#viewKoalas').append(`<tr>
+                                <td>${koala.name}</td>
+                                <td>${koala.gender}</td>
+                                <td>${koala.age}</td>
+                                <td>${koala.ready_to_transfer}</td>
+                                <td>${koala.notes}</td>
+                                <td>
+                                  <button class="ready-button">Ready For Transfer</button>
+                                </td>
+                                <td>
+                                  <button class="delete-button">Delete</button>
+                                </td>
+                              </tr>`)
+    }
+    
+    
+  }).catch(function (error) {
+    console.log('Returning this error:', error);
+    
+  })
+} // end getKoalas  
 
 function saveKoala(){
   // ajax call to server to get koalas
