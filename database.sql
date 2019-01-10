d62d2dc9a568f6befe1726323a929cedeeab7f83
Matt Kleven <klevennb115@gmail.com>
2019-01-10 13:03:28 -0600

    fixed again

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@ CREATE TABLE "koalas" (
 	"notes" VARCHAR(60) DEFAULT ''
 );
 
-INSERT INTO "koalas" (name", "gender", "age", "ready_to_transfer", "notes")
+INSERT INTO "koalas" ("name", "gender", "age", "ready_to_transfer", "notes")
 VALUES ('Doug', 'M', 3, false, 'Stoic'),
 ('Chad', 'F', 19, true, 'Conspiracy theorist'),
 ('Patty Wagstaff', 'M', 1, false, 'Eucalyptus addiction'),
